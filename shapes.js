d084103432b8e7a7a4b09edc2cbc254890b04d03
Santosh Sivan <103419220+SantoshSivan2004@users.noreply.github.com>
2025-05-16 16:29:51 -0500

    Update shapes.js

--- shapes.js
+++ shapes.js
@@ -1,5 +1,5 @@
-import * as THREE from "../node_modules/three/build/three.module.js";
-import { OrbitControls } from 'https://unpkg.com/three@0.160.1/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
 
 function createScene(containerId, shapetype)
 {
